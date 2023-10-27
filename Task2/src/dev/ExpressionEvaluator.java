b1db2074705e35a6bacfac1b21ed1800037e66e0
samarth36483 <raipuriyasamarth@gmail.com>
2023-10-28 00:28:14 +0530

    Added multithreading

--- Task2/src/dev/ExpressionEvaluator.java
+++ Task2/src/dev/ExpressionEvaluator.java
@@ -27,37 +27,59 @@ public class ExpressionEvaluator {
 	    	  String exp = line;
 	    	  q.add(exp);
 	      }
-//	      reader.close();
 	      evaluateExpression(q);
 	    }
 	    catch(IOException e){
 	      e.printStackTrace();
 	    }
 	}
-	public static void evaluateExpression(ArrayList<String> list) {
-		int N = list.size();
-	    	for(String str : list) {
-	    		try {
-	    			String encodedVal = URLEncoder.encode(str, StandardCharsets.UTF_8.toString());
-		    		String apiUrl = "https://api.mathjs.org/v4/?expr=" + encodedVal;
-		    		//System.out.println(apiUrl);
-		    		URL url = new URL(apiUrl);
-		    		//System.out.println(url);
-		    		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-		    		if(connection.getResponseCode() == 200) {
-		    			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		    			String result = reader.readLine();
-		    			System.out.println(str + " => " + result);
-		    			reader.close();
-		    			connection.disconnect();
-		    		}
-		    		else {
-		    			System.out.println(str + " => Error " + connection.getResponseCode() + connection.getResponseMessage());
-		    		}
-	    		}
-	    		catch(IOException e){
-	    			System.out.println(e.getMessage());
-	    	    }
-	    	}
+	public static void evaluateExpression(ArrayList<String> expressions) {
+		for(String expression : expressions) {
+			Thread evaluateThread = new Thread(() -> processExpression(expression));
+			evaluateThread.start();
+			try {
+				evaluateThread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public static void processExpression(String str) {
+		try {
+			String encodedVal = URLEncoder.encode(str, StandardCharsets.UTF_8.toString());
+    		String apiUrl = "https://api.mathjs.org/v4/?expr=" + encodedVal;
+    		URL url = new URL(apiUrl);
+    		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    		if(connection.getResponseCode() == 200) {
+    			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+    			String result = reader.readLine();
+    			System.out.println(str + " => " + result);
+    			reader.close();
+    			connection.disconnect();
+    		}
+    		else {
+    			System.out.println(str + " => Error " + connection.getResponseCode() + connection.getResponseMessage());
+    		}
+		}
+		catch(IOException e){
+			System.out.println(e.getMessage());
+	    }
+
 	  }
 }
+
+/*
+ * Regarding the requirement that the API supports only 50 requests per second per client and my application 
+ * has to implement 500 requests per second. There can be 3 scenerios for this:
+ * 1. Using single machine
+ * If we are running code on single machine, the server identifies client with ip address. Therefore we can't make 
+ * 500 calls if ip rate limit is of 50 calls.
+ * 
+ * 2.
+ * Using single machine
+ * We can negotiate with the server to increase the limit to 500.
+ * 
+ * 3. Using multi machine
+ * We can use multiple machines and load balancers in round robin fashion.
+ * 
+ * */
